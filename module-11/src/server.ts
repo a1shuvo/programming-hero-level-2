e5719d7f1c0fe82d512444e77df151ec5214ab29
a1shuvo <a1shuvosaha@gmail.com>
2025-11-24 21:31:02 +0600

    Added module 11 node js api routes and difference between GET & POST method exercise

--- module-11/src/server.ts
+++ module-11/src/server.ts
@@ -5,6 +5,7 @@ const server: Server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
     console.log("Server is running...");
 
+    // root route
     if (req.url == "/" && req.method == "GET") {
       res.writeHead(200, { "content-type": "application/json" });
       res.end(
@@ -13,6 +14,45 @@ const server: Server = http.createServer(
           path: req.url,
         })
       );
+    }
+
+    // health route
+    if (req.url == "/api" && req.method == "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Health status is ok",
+          path: req.url,
+        })
+      );
+    }
+
+    // user post route
+    if (req.url == "/api/users" && req.method == "POST") {
+      // const user = {
+      //   id: 123,
+      //   name: "Shuvo",
+      // };
+      // res.writeHead(200, { "content-type": "application/json" });
+      // res.end(JSON.stringify(user));
+
+      let body = "";
+
+      // listen for data chunk
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+
+      req.on("end", () => {
+        try {
+          const parsedBody = JSON.parse(body);
+          console.log(parsedBody);
+          console.log("Catching current changes");
+          res.end(JSON.stringify(parsedBody));
+        } catch (error: any) {
+          console.log(error?.message);
+        }
+      });
     }
   }
 );
