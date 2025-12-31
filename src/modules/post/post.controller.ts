3bddd13963cf86dd5db69e0b1e84f5e5471c2264
Fahim Ahammed Firoz <fahimahammed.cse@gmail.com>
2025-12-31 14:58:48 +0600

    feat: enhance getAllPost service to support filtering by isFeatured, status, and authorId

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { postService } from "./post.service";
+import { PostStatus } from "../../../generated/prisma/enums";
 
 const createPost = async (req: Request, res: Response) => {
     try {
@@ -27,7 +28,21 @@ const getAllPost = async (req: Request, res: Response) => {
 
         const tags = req.query.tags ? (req.query.tags as string).split(",") : [];
 
-        const result = await postService.getAllPost({ search: searchString, tags })
+
+        // true or false
+        const isFeatured = req.query.isFeatured
+            ? req.query.isFeatured === 'true'
+                ? true
+                : req.query.isFeatured === 'false'
+                    ? false
+                    : undefined
+            : undefined
+
+        const status = req.query.status as PostStatus | undefined
+
+        const authorId = req.query.authorId as string | undefined
+
+        const result = await postService.getAllPost({ search: searchString, tags, isFeatured, status, authorId })
         res.status(200).json(result)
     } catch (e) {
         res.status(400).json({
