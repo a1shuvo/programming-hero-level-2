dcdd4eaf1f76c2e26099774fba88e73eddb94f76
a1shuvo <a1shuvosaha@gmail.com>
2026-01-02 20:46:32 +0600

    Implemented email and password authentication setup with better auth

--- module-23/prisma-blog-app/src/app.ts
+++ module-23/prisma-blog-app/src/app.ts
@@ -1,8 +1,11 @@
+import { toNodeHandler } from "better-auth/node";
 import express, { Application } from "express";
+import { auth } from "./lib/auth";
 import { PostRouter } from "./modules/post/post.router";
 
 const app: Application = express();
 
+app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use(express.json());
 
 app.use("/posts", PostRouter);
