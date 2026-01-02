119d15222f613200be6cdc455466c1478fe2f3b1
Fahim Ahammed Firoz <fahimahammed.cse@gmail.com>
2026-01-02 15:19:51 +0600

    feat: add seedAdmin script for initializing admin user in the database

---
+++ src/scripts/seedAdmin.ts
@@ -0,0 +1,34 @@
+import { prisma } from "../lib/prisma";
+import { UserRole } from "../middlewares/auth";
+
+async function seedAdmin() {
+    try {
+        const adminData = {
+            name: "Admin Saheb",
+            email: "admin@admin.com",
+            role: UserRole.ADMIN,
+            password: "admin1234"
+        }
+        // check user exist on db or not
+        const existingUser = await prisma.user.findUnique({
+            where: {
+                email: adminData.email
+            }
+        });
+
+        if (existingUser) {
+            throw new Error("User already exists!!");
+        }
+
+        const signUpAdmin = await fetch("http://localhost:3000/api/auth/sign-up/email", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(adminData)
+        })
+
+    } catch (error) {
+        console.error(error);
+    }
+}
