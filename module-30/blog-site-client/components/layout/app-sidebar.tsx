2dc14966b4388118cced39d981badb230323e247
a1shuvo <a1shuvosaha@gmail.com>
2026-01-22 21:48:57 +0600

    Implemented role based dashboard sidebar using parallel routes

--- module-30/blog-site-client/components/layout/app-sidebar.tsx
+++ module-30/blog-site-client/components/layout/app-sidebar.tsx
@@ -11,43 +11,44 @@ import {
   SidebarMenuItem,
   SidebarRail,
 } from "@/components/ui/sidebar";
+import { adminRoutes } from "@/routes/adminRoutes";
+import { userRoutes } from "@/routes/userRoutes";
+import { Routes } from "@/types";
+import Link from "next/link";
 
-// This is sample data.
-const data = {
-  versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
-  navMain: [
-    {
-      title: "Getting Started",
-      url: "#",
-      items: [
-        {
-          title: "Write Blog",
-          url: "/dashboard/write-blog",
-          isActive: true,
-        },
-        {
-          title: "Analytics",
-          url: "/dashboard/analytics",
-        },
-      ],
-    },
-  ],
-};
+export function AppSidebar({
+  user,
+  ...props
+}: {
+  user: { role: string } & React.ComponentProps<typeof Sidebar>;
+}) {
+  let routes: Routes[] = [];
+  switch (user.role) {
+    case "admin":
+      routes = adminRoutes;
+      break;
 
-export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+    case "user":
+      routes = userRoutes;
+      break;
+
+    default:
+      routes = [];
+      break;
+  }
   return (
     <Sidebar {...props}>
       <SidebarContent>
         {/* We create a SidebarGroup for each parent. */}
-        {data.navMain.map((item) => (
+        {routes.map((item) => (
           <SidebarGroup key={item.title}>
             <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
             <SidebarGroupContent>
               <SidebarMenu>
                 {item.items.map((item) => (
                   <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild isActive={item.isActive}>
-                      <a href={item.url}>{item.title}</a>
+                    <SidebarMenuButton asChild>
+                      <Link href={item.url}>{item.title}</Link>
                     </SidebarMenuButton>
                   </SidebarMenuItem>
                 ))}
---
+++ module-30/blog-site-client/routes/userRoutes.ts
@@ -0,0 +1,13 @@
+import { Routes } from "@/types";
+
+export const userRoutes: Routes[] = [
+  {
+    title: "Blog Management",
+    items: [
+      {
+        title: "Write Blog",
+        url: "/write-blog",
+      },
+    ],
+  },
+];
