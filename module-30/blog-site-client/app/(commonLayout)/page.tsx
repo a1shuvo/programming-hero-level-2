72e1edcbad5a1c1d52e3bb7c8eaf41d02a05c8d2
a1shuvo <a1shuvosaha@gmail.com>
2026-01-25 20:37:23 +0600

    Created blog post service and fetched dummy posts data

--- module-30/blog-site-client/app/(commonLayout)/page.tsx
+++ module-30/blog-site-client/app/(commonLayout)/page.tsx
@@ -1,6 +1,9 @@
 import { Button } from "@/components/ui/button";
+import { blogService } from "@/services/blog.service";
 
 export default async function Home() {
+  const { data } = await blogService.getBlogPosts();
+  console.log(data);
   return (
     <div>
       <Button variant={"outline"}>Click Me</Button>
