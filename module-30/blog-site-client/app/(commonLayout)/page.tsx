1d828ab9522e8d40def548f4ed90eeb291712b20
a1shuvo <a1shuvosaha@gmail.com>
2026-01-25 22:14:42 +0600

    Implemented get blog posts with search params and config options

--- module-30/blog-site-client/app/(commonLayout)/page.tsx
+++ module-30/blog-site-client/app/(commonLayout)/page.tsx
@@ -1,12 +1,23 @@
-import { Button } from "@/components/ui/button";
+import BlogCard from "@/components/modules/homepage/BlogCard";
 import { blogService } from "@/services/blog.service";
+import { BlogPost } from "@/types";
 
 export default async function Home() {
-  const { data } = await blogService.getBlogPosts();
-  console.log(data);
+  const { data } = await blogService.getBlogPosts(
+    {
+      isFeatured: false,
+      search: "post 1",
+    },
+    {
+      revalidate: 10,
+      // cache: "no-store",
+    },
+  );
   return (
-    <div>
-      <Button variant={"outline"}>Click Me</Button>
+    <div className="grid grid-cols-3 max-w-7xl mx-auto gap-5 px-4">
+      {data?.data?.map((post: BlogPost) => (
+        <BlogCard key={post.id} post={post} />
+      ))}
     </div>
   );
 }
