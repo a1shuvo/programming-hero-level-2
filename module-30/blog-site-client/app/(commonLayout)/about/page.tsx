8e6b0e49a56d47c570f97e4a9d39fe989e9dc12c
a1shuvo <a1shuvosaha@gmail.com>
2026-01-26 16:26:45 +0600

    Implemented server function or action for fetching data in client component via server coponent

--- module-30/blog-site-client/app/(commonLayout)/about/page.tsx
+++ module-30/blog-site-client/app/(commonLayout)/about/page.tsx
@@ -1,10 +1,29 @@
-export const dynamic = "force-dynamic";
-export default async function AboutPage() {
+"use client";
+
+import { getBlogs } from "@/actions/blog.action";
+import { useEffect, useState } from "react";
+
+// export const dynamic = "force-dynamic";
+
+export default function AboutPage() {
   // For simulating load time
-  await new Promise((resolve) => setTimeout(resolve, 4000));
+  // await new Promise((resolve) => setTimeout(resolve, 4000));
 
   // For simulating error
   // throw new Error("Something went wrong!");
+  const [data, setData] = useState();
+  const [error, setError] = useState<{ message: string } | null>(null);
+
+  console.log(data);
+  console.log(error);
+
+  useEffect(() => {
+    (async () => {
+      const { data, error } = await getBlogs();
+      setData(data);
+      setError(error);
+    })();
+  }, []);
 
   return <div>This is about page component</div>;
 }
